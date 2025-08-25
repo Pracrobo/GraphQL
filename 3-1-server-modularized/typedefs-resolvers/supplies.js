1a69554fc1c4329f10862b0843fcc69f6f0dd940
Pracrobo <cpu180621@gmail.com>
2025-08-25 13:14:24 +0900

    3-1 server 구성 요소 모듈화

---
+++ 3-1-server-modularized/typedefs-resolvers/supplies.js
@@ -0,0 +1,21 @@
+const dbWorks = require("../dbWorks");
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Supply {
+    id: String
+    team: Int
+  }
+`;
+const resolvers = {
+  Query: {
+    supplies: (parent, args) => dbWorks.getSupplies(args),
+  },
+  Mutation: {
+    deleteSupply: (parent, args) => dbWorks.deleteItem("supplies", args),
+  },
+};
+module.exports = {
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+};
